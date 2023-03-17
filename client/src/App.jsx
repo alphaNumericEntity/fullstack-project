8aa64385a0fa14205d4cb5efc60634bc6e303dda
juan <71961814+alphaNumericEntity@users.noreply.github.com>
2023-03-17 23:16:58 +0900

    add logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,17 @@ import './App.css'
 function App() {
 
   return (
-    <div className="bg-red-500">test</div>
+   <div>
+    <header className='p-4'>
+      <a href="" className='flex items-center gap-1'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-9 h-9 -rotate-90">
+               <path d="M3.105 2.289a.75.75 0 00-.826.95l1.414 4.925A1.5 1.5 0 005.135 9.25h6.115a.75.75 0 010 1.5H5.135a1.5 1.5 0 00-1.442 1.086l-1.414 4.926a.75.75 0 00.826.95 28.896 28.896 0 0015.293-7.154.75.75 0 000-1.115A28.897 28.897 0 003.105 2.289z" />
+            </svg>
+            <span className='font-bold text-xl'> airbnd</span>
+
+      </a>
+    </header>
+   </div>
   )
 }
 
