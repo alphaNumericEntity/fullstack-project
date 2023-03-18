40b47c970e60171edf7328d79bbb6be892a47869
juan <71961814+alphaNumericEntity@users.noreply.github.com>
2023-03-18 10:07:32 +0900

    search button with color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,11 +19,10 @@ function App() {
         <div>Any week</div>
         <div className="border border-l border-black-300"></div>
         <div>Add guests</div>
-        <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                   <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                 </svg>
-
+        <button className='bg-primary text-white p-2 rounded-full'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-3 h-3">
+              <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
+            </svg>
         </button>
       </div>
     </header>
