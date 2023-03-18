525a22fbb728fe528a793015cf265aa7bebb304a
juan <71961814+alphaNumericEntity@users.noreply.github.com>
2023-03-18 12:04:29 +0900

    add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
 
   return (
    <Routes>
     <Route index element={<IndexPage></IndexPage>}></Route>
+    <Route path='/login' element={<LoginPage></LoginPage>}></Route>
    </Routes>
   )
 }
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function IndexPage() {
     return (<div>
         <header className="p-4 flex justify-between" >
@@ -23,7 +25,7 @@ export default function IndexPage() {
           </div>
     
     
-          <div className="flex items-center  gap-2 border border-gray-300 rounded-full py-2 px-8">
+          <Link to={'/login'} className="flex items-center  gap-2 border border-gray-300 rounded-full py-2 px-8">
             <div className="">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                 <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
@@ -34,7 +36,7 @@ export default function IndexPage() {
                   <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
               </svg>
             </div>
-          </div>
+          </Link>
         </header>
        </div>);
 }
