cc114e7d8d17a031a4efc6ac8575ba7cd5d3ebc2
juan <71961814+alphaNumericEntity@users.noreply.github.com>
2023-03-18 20:38:10 +0900

    login page ui

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,13 @@
 export default function LoginPage() {
-    return 'login page here';
+    return(
+        <div className="mt-4">
+            <h1 className="text-4xl text-center">Login</h1>
+            <form className="max-w-md mx-auto border">
+                <input type="email" placeholder="yur@email.com" />
+                <input type="password" placeholder="password"/>
+                <button>Login</button>
+            </form>
+
+        </div>
+    )
 }
