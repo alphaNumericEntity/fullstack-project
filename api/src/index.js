18ad38efcb236875832d2a39dc01388152969a20
juan <71961814+alphaNumericEntity@users.noreply.github.com>
2023-03-19 10:36:40 +0900

    inform user of successful / unsuccessful registration

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const { json } = require('express');
+const bcryptJs = require('bcryptjs');
 const mongoose = require('mongoose');
 const UserModel = require('./models/User');
 require('dotenv').config()
 
 
+
 const app = express();
-
 app.use(json());
-
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 
 }));
+
+const bcryptSalt =  bcryptJs.genSaltSync(10);
 
 
 mongoose.connect(process.env.MONGO_URL);
@@ -23,19 +25,19 @@ app.get('/test',(req,res)=>{
     res.json('this is the response');
 });
 
-app.post('/register',(req,res)=>{
+app.post('/register', async (req,res)=>{
     const {name,email,password} = req.body;
 
-    UserModel.create({
-        name,
-        email,
-        password,
-    });
-    res.json({
-        name,
-        email,
-        password,
-    });
+    try {
+        const user = await UserModel.create({
+            name,
+            email,
+            password : bcryptJs.hashSync(password, bcryptSalt),
+        });
+        res.status(200).send('User added successfully');
+    } catch (e) {
+        res.status(422).send(e);
+    }
 });
 
 app.listen(4000);
