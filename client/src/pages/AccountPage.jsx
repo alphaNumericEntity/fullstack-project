28559744eeac742682230224a6403edc76f81fb9
juan <71961814+alphaNumericEntity@users.noreply.github.com>
2023-03-23 23:33:52 +0900

    fix unreliable behavior  on acount page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,6 +1,19 @@
+import { useContext } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
+
 export default function AccountPage() {
 
+    const {user, ready} = useContext(UserContext);
+
+    if (!ready) {
+        return 'Loading...'
+    }
+    if (ready && !user) {
+        return (<Navigate to='/login'></Navigate>);
+    }
+
     return (
-        <div> this is account page</div>
+        <div> this is account page for {user.name} </div>
     );
 }
